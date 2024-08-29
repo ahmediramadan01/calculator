318178d3193985bc9683f6aae81b97831c82903b
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2024-08-29 20:13:12 +0300

    Create basic math operations functions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const add = function (a, b) {
+	return parseFloat(a) + parseFloat(b);
+};
+
+const subtract = function (a, b) {
+	return parseFloat(a) - parseFloat(b);
+};
+
+const multiply = function (a, b) {
+	return parseFloat(a) * parseFloat(b);
+};
+
+const divide = function (a, b) {
+	if (parseFloat(b) === 0) return "UNDEFINED";
+	return parseFloat(a) / parseFloat(b);
+};
